71e8eebacfd1b950830832a659b747fe983d94e5
lagunatlt <lagunatlt@yandex.ru>
2020-03-22 01:55:08 +0400

    ok

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('.work-slider-1').slick({
+	infinite: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	// nextArrow: $('.next-arrow-1'),
+	// prevArrow: $('.prev-arrow-1'),
+	autoplay: true,
+	autoplaySpeed: 2000,
+});
+
+$('.work-slider-2').slick({
+	infinite: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	// nextArrow: $('.next-arrow-1'),
+	// prevArrow: $('.prev-arrow-1'),
+	autoplay: true,
+	autoplaySpeed: 2000,
+});
+$('.services-slider').slick({
+	infinite: false,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	arrows: true,
+	// nextArrow: $('.next-arrow-1'),
+	// prevArrow: $('.prev-arrow-1'),
+	autoplay: true,
+	autoplaySpeed: 2000,
+	variableWidth: true,
+});
