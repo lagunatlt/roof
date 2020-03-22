5de2e9970f1a7f2391968ed01c95450857ae4f1b
lagunatlt <lagunatlt@yandex.ru>
2020-03-22 17:11:28 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -19,6 +19,16 @@ $('.work-slider-2').slick({
 	autoplay: true,
 	autoplaySpeed: 2000,
 });
+$('.work-slider-3').slick({
+	infinite: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: true,
+	// nextArrow: $('.next-arrow-1'),
+	// prevArrow: $('.prev-arrow-1'),
+	autoplay: true,
+	autoplaySpeed: 2000,
+});
 $('.services-slider').slick({
 	infinite: false,
 	slidesToShow: 3,
@@ -30,3 +40,22 @@ $('.services-slider').slick({
 	autoplaySpeed: 2000,
 	variableWidth: true,
 });
+
+
+/* accordion */
+let accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+	accordions[i].onclick = function () {
+		this.classList.toggle('is-open');
+		this.classList.toggle('rotate');
+
+		let content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	};
+}
+/* ----------- */
