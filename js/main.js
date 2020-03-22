c690a2972630a63c60ea344500371ec54059418f
lagunatlt <lagunatlt@yandex.ru>
2020-03-23 00:02:02 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// AOS.init();
+
 $('.work-slider-1').slick({
 	infinite: false,
 	slidesToShow: 1,
@@ -69,3 +71,61 @@ document.addEventListener('click', function (e) {
 	}
 })
 /* ---------- */
+$(document).ready(function () {
+	setTimeout(function() {
+		$("#hidden-promo").fancybox().trigger('click');
+	}, 60000)
+});
+
+/* -------вызов модалки по достижении блока------- */
+// var offset = 600;
+// $(window).scroll(function () {
+// 	var scrolltop = $(this).scrollTop();
+// 	$('.how-work').each(function () {
+// 		if (scrolltop >= $(this).offset().top - offset) {
+// 			// $(this).removeClass('hide');
+// 			$("#hidden-promo").fancybox().trigger('click');
+// 		}
+// 	});
+// });
+/* ------------------ */
+
+/* mask */
+window.addEventListener("DOMContentLoaded", function () {
+	[].forEach.call(document.querySelectorAll('.tel'), function (input) {
+		var keyCode;
+
+		function mask(event) {
+			event.keyCode && (keyCode = event.keyCode);
+			var pos = this.selectionStart;
+			if (pos < 3) event.preventDefault();
+			var matrix = "+7 (___) ___-__-__",
+				i = 0,
+				def = matrix.replace(/\D/g, ""),
+				val = this.value.replace(/\D/g, ""),
+				new_value = matrix.replace(/[_\d]/g, function (a) {
+					return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+				});
+			i = new_value.indexOf("_");
+			if (i != -1) {
+				i < 5 && (i = 3);
+				new_value = new_value.slice(0, i)
+			}
+			var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+				function (a) {
+					return "\\d{1," + a.length + "}"
+				}).replace(/[+()]/g, "\\$&");
+			reg = new RegExp("^" + reg + "$");
+			if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+			if (event.type == "blur" && this.value.length < 5) this.value = ""
+		}
+
+		input.addEventListener("input", mask, false);
+		input.addEventListener("focus", mask, false);
+		input.addEventListener("blur", mask, false);
+		input.addEventListener("keydown", mask, false)
+
+	});
+
+});
+/* ----------- */
