9408861da53b6f337e036a17bc112aeee085f84d
lagunatlt <lagunatlt@yandex.ru>
2020-03-25 07:13:02 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -19,6 +19,32 @@ $('.work-slider-1').slick({
 	// prevArrow: $('.prev-arrow-1'),
 	// autoplay: true,
 	// autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 961,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 721,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 641,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 320,
+			settings: {
+				arrows: false,
+			}
+		}
+	]
 });
 
 $('.work-slider-2').slick({
@@ -30,6 +56,32 @@ $('.work-slider-2').slick({
 	// prevArrow: $('.prev-arrow-1'),
 	// autoplay: true,
 	// autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 961,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 721,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 641,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 320,
+			settings: {
+				arrows: false,
+			}
+		}
+	]
 });
 $('.work-slider-3').slick({
 	infinite: false,
@@ -40,6 +92,32 @@ $('.work-slider-3').slick({
 	// prevArrow: $('.prev-arrow-1'),
 	// autoplay: true,
 	// autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 961,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 721,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 641,
+			settings: {
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 320,
+			settings: {
+				arrows: false,
+			}
+		}
+	]
 });
 $('.services-slider').slick({
 	infinite: false,
@@ -64,10 +142,19 @@ $('.services-slider').slick({
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
+				arrows: false,
 			}
 		},
 		{
 			breakpoint: 641,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				arrows: false,
+			}
+		},
+		{
+			breakpoint: 320,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
@@ -163,3 +250,39 @@ window.addEventListener("DOMContentLoaded", function () {
 
 });
 /* ----------- */
+
+/* ---numbers--- */
+$(function () {
+	var num = $(".number");
+	num.each(function (indx, el) {
+		var max = $(el).data("max");
+		var duration = 2000;
+		var visibility = checkViewport(el);
+		$(el).on("animeNum", function () {
+			$({ n: 0 }).animate({ n: max }, {
+				easing: "linear",
+				duration: duration,
+				step: function (now, fx) {
+					$(el).html(now | 0)
+				}
+			})
+		}).data("visibility", visibility);
+		visibility && $(el).trigger("animeNum")
+	});
+
+	function checkViewport(el) {
+		var H = document.documentElement.clientHeight,
+			h = el.scrollHeight,
+			pos = el.getBoundingClientRect();
+		return pos.top + h > 0 && pos.bottom - h < H
+	}
+	$(window).scroll(function () {
+		num.each(function (indx, el) {
+			var visibility = checkViewport(el);
+			el = $(el);
+			var old = el.data("visibility");
+			old != visibility && el.data("visibility", visibility) && !old && el.trigger("animeNum")
+		})
+	})
+});
+/* --------------- */
